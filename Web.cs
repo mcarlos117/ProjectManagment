1559c912117c3ad2ba31d757fb5b5b26ec0c691f
mcarlos117 <miguelcarlos117@yahoo.com>
2016-12-15 15:01:43 -0600

    Update Web.cs

--- Web.cs
+++ Web.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class Web : MonoBehaviour {
 
     
-        public string url = "https://easel1.fulgentcorp.com/bifrost/ws.php?json=[{%22action%22:%22run_sql%22},{%22query%22:%22%20INSERT%20INTO%20`Logins`(`login_count`)%20VALUES%20(0)%22},{%22session_key%22:%225baac3e06d00d2fc8086dddd0febaea5e7bfcc72bc197823ff845d9a67d63e9d%22}]";
+        public string url = "https://easel1.fulgentcorp.com/bifrost/ws.php?json=[{%22action%22:%22run_sql%22},{%22query%22:%22INSERT%20INTO%20`Logins`(`login_count`)%20VALUES%20(0)%22},{%22session_key%22:%2210ef46780a866cbbe95bddeb46432cbd67177ecde443d822e27780ce4fe36142%22}]";
 
         public IEnumerator DoWWW()
         {
